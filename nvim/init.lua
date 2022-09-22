25ed26de16de7faa9eb60246b27b67c30c8f503f
nahuel <lescanoagustin10nahuel@gmail.com>
2022-09-22 14:37:12 -0300

    Modificando la apariencia de neovim más plugins.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,4 @@
--- References:
+-- References
 -- https://github.com/numToStr/dotfiles/tree/master/neovim/.config/nvim/
 -- https://www.youtube.com/watch?v=m62UCkdQ8Ck&t=698s
 
@@ -91,84 +91,68 @@ map('n', '<leader>h', '<CMD>split<CR>')
 -- Open alacritty
 map('n', '<leader>t', '<CMD>term<CR>')
 
+-- Load recent sessions
+map('n', '<leader>sl', '<CMD>SessionLoad<CR>')
+
+-- Keybindings for telescope
+map('n', '<leader>fr', '<CMD>Telescope oldfiles<CR>')
+map('n', '<leader>ff', '<CMD>Telescope find_files<CR>')
+map('n', '<leader>fb', '<CMD>Telescope file_browser<CR>')
+map('n', '<leader>fw', '<CMD>Telescope live_grep<CR>')
+map('n', '<leader>ht', '<CMD>Telescope colorscheme<CR>')
+
 require('lualine').setup()
 
--- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-require'lspconfig'.pyright.setup{
-    capabilities = capabilities,
-    on_attach = function() 
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = 0 })
-        vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { buffer = 0 })
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = 0 })
-        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { buffer = 0 })
-        vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, { buffer = 0 })
-        vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, { buffer = 0 })
-    end,
+-- Dashboard
+local db = require('dashboard')
+local home = os.getenv('HOME')
+
+db.default_banner = {
+  '',
+  '',
+  ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
+  ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
+  ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
+  ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
+  ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
+  ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
+  '',
+  ' [ TIP: To exit Neovim, just power off your computer. ] ',
+  '',
 }
-
-opt.completeopt={ "menu" ,"menuone" ,"noselect" }
-
--- Setup nvim-cmp.
-local cmp = require'cmp'
-
-cmp.setup({
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-       require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-    end,
-  },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' }, -- For luasnip users.
-  }, {
-    { name = 'buffer' },
-  })
-})
-
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = 'buffer' },
-  })
-})
-
-  --Debugging
-vim.keymap.set("n","<F5>", "<CMD>lua require'dap'.continue()<CR>")
-vim.keymap.set("n","<F10>", "<CMD>lua require'dap'.step_over()<CR>")
-vim.keymap.set("n","<F11>", "<CMD>lua require'dap'.step_into()<CR>")
-vim.keymap.set("n","<F12>", "<CMD>lua require'dap'.step_out()<CR>")
-vim.keymap.set("n","<leader>b", "<CMD>lua require'dap'.toggle_breakpoint()<CR>")
-vim.keymap.set("n","<leader>B", "<CMD>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
-vim.keymap.set("n","<leader>lp", "<CMD>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>")
-
-require("dapui").setup()
+db.preview_file_height = 11
+db.preview_file_width = 70
+db.custom_center = {
+    {icon = '  ',
+    desc = 'Recent sessions                         ',
+    shortcut = 'SPC s l',
+    action ='SessionLoad'},
+    {icon = '  ',
+    desc = 'Find recent files                       ',
+    action = 'Telescope oldfiles',
+    shortcut = 'SPC f r'},
+    {icon = '  ',
+    desc = 'Find files                              ',
+    action = 'Telescope find_files find_command=rg,--hidden,--files',
+    shortcut = 'SPC f f'},
+    {icon = '  ',
+    desc ='File browser                            ',
+    action =  'Telescope file_browser',
+    shortcut = 'SPC f b'},
+    {icon = '  ',
+    desc = 'Find word                               ',
+    action = 'Telescope live_grep',
+    shortcut = 'SPC f w'},
+    {icon = '  ',
+    desc = 'Load new theme                          ',
+    action = 'Telescope colorscheme',
+    shortcut = 'SPC h t'},
+  }
+db.custom_footer = { '', '🎉 If I\'m using Neovim, then my Emacs config must be broken!' }
+db.session_directory = "/home/dt/.config/nvim/session"
 
 -- COLORSCHEMES
-local ok, _ = pcall(vim.cmd, 'colorscheme base16-ayu-dark')
-
--- Automatically install and set up packer.vim
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd [[packadd packer.nvim]]
-end
+local ok, _ = pcall(vim.cmd, 'colorscheme base16-onedark')
 
 -- PLUGINS
 return require('packer').startup(function()
@@ -180,6 +164,48 @@ return require('packer').startup(function()
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
+
+    -- Telescope and related stuff
+    use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
+    use { "nvim-telescope/telescope-file-browser.nvim",
+        config = function()
+        require("telescope").setup {
+          extensions = {
+            file_browser = {
+              theme = "ivy",
+              -- disables netrw and use telescope-file-browser in its place
+              hijack_netrw = true,
+              mappings = {
+                ["i"] = {
+                  -- your custom insert mode mappings
+                },
+                ["n"] = {
+                  -- your custom normal mode mappings
+                },
+              },
+            },
+          },
+        }
+        end
+    }
+    -- To get telescope-file-browser loaded and working with telescope,
+    -- you need to call load_extension, somewhere after setup function:
+    require("telescope").load_extension "file_browser"
+
+    -- Which key
+    use {
+      "folke/which-key.nvim",
+      config = function()
+        require("which-key").setup { }
+      end
+    }
+
+    -- Dashboard is a nice start screen for nvim
+    use 'glepnir/dashboard-nvim'
 
     -- File management
     use 'vifm/vifm.vim'
@@ -193,33 +219,6 @@ return require('packer').startup(function()
     use 'kovetskiy/sxhkd-vim'
     use 'vim-python/python-syntax'
 
-    -- Configurations for nvim LSP
-    use 'neovim/nvim-lspconfig'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/nvim-cmp'
-
-    -- For vsnip users.
-    use 'hrsh7th/cmp-vsnip'
-    use 'hrsh7th/vim-vsnip'
-
-    -- For luasnip users.
-    use 'L3MON4D3/LuaSnip'
-    use 'saadparwaiz1/cmp_luasnip'
-
-    -- For ultisnips users.
-    use 'SirVer/ultisnips'
-    use 'quangnguyen30192/cmp-nvim-ultisnips'
-
-    -- For snippy users.
-    use 'dcampos/nvim-snippy'
-    use 'dcampos/cmp-snippy'
-
-    --Debugging
-    use 'mfussenegger/nvim-dap'
-    use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
-
     -- Junegunn Choi Plugins --
     use 'junegunn/goyo.vim'
     use 'junegunn/limelight.vim'
@@ -232,9 +231,4 @@ return require('packer').startup(function()
     -- Other stuff
     use 'frazrepo/vim-rainbow'
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
 end)
